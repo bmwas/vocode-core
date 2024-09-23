0225e6eb1de6cd013142aa3ff61d17c70193a85e
Benson <benson.mwangi@gmail.com>
2024-09-23 08:29:32 -0500

    enhance cc action ..

--- vocode/streaming/action/contact_center_query.py
+++ vocode/streaming/action/contact_center_query.py
@@ -49,7 +49,7 @@ class GetPhoneAndQueryContactCenterAction(
     description: str = """
     Queries caller information such as; name, phone number, and email addresses
     Query user name, phone, address and email if this is a new call
-    Used at the beginning of a call, if a caller is returning a call 
+    Query should be used at the beginning of a call, if a caller is returning a call 
     Query or search for caller info if caller is returning a call.
     """
     response_type: Type[QueryContactCenterResponse] = QueryContactCenterResponse
