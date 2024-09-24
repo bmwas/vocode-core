4aca961bfaf6dbf0fc28164603fb8193dcadfa59
Benson <benson.mwangi@gmail.com>
2024-09-24 18:51:50 -0500

    save on cc changes ..

--- vocode/streaming/action/contact_center_save.py
+++ vocode/streaming/action/contact_center_save.py
@@ -27,7 +27,7 @@ class SaveContactCenterResponse(BaseModel):
 
 
 class SaveContactToContactCenterActionConfig(
-    VocodeActionConfig, type="action_get_phone_and_save_contact_center"
+    VocodeActionConfig, type="action_save_contact_center"
 ):
     def action_attempt_to_string(self, input: ActionInput) -> str:
         return "Attempting to save caller contact information to contact center"
