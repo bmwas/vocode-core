79c9928c0705201ccefc2f042c1ced47c2b735fe
Benson <benson.mwangi@gmail.com>
2024-09-24 19:05:06 -0500

    save on cc changes ..

--- vocode/streaming/action/contact_center_save.py
+++ vocode/streaming/action/contact_center_save.py
@@ -1,177 +1,100 @@
-import re
-import json
-import os
-import secrets
-from typing import Type, Optional
+from typing import Literal, Optional, Type, Union, get_args
 
 from loguru import logger
-from pydantic import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 
-from vocode.streaming.action.phone_call_action import TwilioPhoneConversationAction
+from vocode.streaming.action.phone_call_action import (
+    TwilioPhoneConversationAction,
+    VonagePhoneConversationAction,
+)
 from vocode.streaming.models.actions import ActionConfig as VocodeActionConfig
 from vocode.streaming.models.actions import ActionInput, ActionOutput
 from vocode.streaming.utils.async_requester import AsyncRequestor
-from vocode.streaming.utils.state_manager import TwilioPhoneConversationStateManager
+from vocode.streaming.utils.phone_numbers import sanitize_phone_number
+from vocode.streaming.utils.state_manager import (
+    TwilioPhoneConversationStateManager,
+    VonagePhoneConversationStateManager,
+)
 
-import aiohttp
+import os
+import json
+import secrets
 
 
-class SaveContactParameters(BaseModel):
+class AddToContactCenterEmptyParameters(BaseModel):
+    pass
+
+
+class AddToContactCenterRequiredParameters(BaseModel):
+    phone_number: str = Field(..., description="The phone number of the caller")
     caller_name: str = Field(..., description="The name of the caller")
     email_address: str = Field(..., description="The email address of the caller")
 
 
-class SaveContactCenterResponse(BaseModel):
+AddToContactCenterParameters = Union[
+    AddToContactCenterEmptyParameters, AddToContactCenterRequiredParameters
+]
+
+
+class AddToContactCenterResponse(BaseModel):
     success: bool
-    result: Optional[dict]
+    message: Optional[str] = None
 
 
-class SaveContactToContactCenterActionConfig(
-    VocodeActionConfig, type="action_save_contact_center"
-):
+class AddToContactCenterVocodeActionConfig(VocodeActionConfig, type="action_add_to_contact_center"):  # type: ignore
+    phone_number: Optional[str] = Field(
+        None, description="The phone number of the caller"
+    )
+    caller_name: Optional[str] = Field(
+        None, description="The name of the caller"
+    )
+    email_address: Optional[str] = Field(
+        None, description="The email address of the caller"
+    )
+
+    def get_phone_number(self, input: ActionInput) -> str:
+        if isinstance(input.params, AddToContactCenterRequiredParameters):
+            return input.params.phone_number
+        elif isinstance(input.params, AddToContactCenterEmptyParameters):
+            assert self.phone_number, "phone number must be set"
+            return self.phone_number
+        else:
+            raise TypeError("Invalid input params type")
+
+    def get_caller_name(self, input: ActionInput) -> str:
+        if isinstance(input.params, AddToContactCenterRequiredParameters):
+            return input.params.caller_name
+        elif isinstance(input.params, AddToContactCenterEmptyParameters):
+            assert self.caller_name, "caller name must be set"
+            return self.caller_name
+        else:
+            raise TypeError("Invalid input params type")
+
+    def get_email_address(self, input: ActionInput) -> str:
+        if isinstance(input.params, AddToContactCenterRequiredParameters):
+            return input.params.email_address
+        elif isinstance(input.params, AddToContactCenterEmptyParameters):
+            assert self.email_address, "email address must be set"
+            return self.email_address
+        else:
+            raise TypeError("Invalid input params type")
+
     def action_attempt_to_string(self, input: ActionInput) -> str:
-        return "Attempting to save caller contact information to contact center"
+        return f"Attempting to add contact to contact center"
 
     def action_result_to_string(self, input: ActionInput, output: ActionOutput) -> str:
-        if (
-            output.response
-            and output.response.result
-            and "agent_message" in output.response.result
-        ):
-            return output.response.result["agent_message"]
-        elif output.response.success:
-            return "Operation was successful, but no message provided."
+        assert isinstance(output.response, AddToContactCenterResponse)
+        if output.response.success:
+            action_description = "Successfully added contact to contact center"
         else:
-            return "Failed to save contact information"
+            action_description = f"Failed to add contact: {output.response.message}"
+        return action_description
 
 
-class SaveContactToContactCenterAction(
-    TwilioPhoneConversationAction[
-        SaveContactToContactCenterActionConfig,
-        SaveContactParameters,
-        SaveContactCenterResponse,
-    ]
-):
-    description: str = """
-    Saves caller information such as name, phone number, and email address to the contact center.
-    """
-    response_type: Type[SaveContactCenterResponse] = SaveContactCenterResponse
-    conversation_state_manager: TwilioPhoneConversationStateManager
-
-    @property
-    def parameters_type(self) -> Type[SaveContactParameters]:
-        return SaveContactParameters
-
-    def __init__(
-        self,
-        action_config: SaveContactToContactCenterActionConfig,
-    ):
-        super().__init__(
-            action_config,
-            quiet=False,
-            is_interruptible=False,
-            should_respond="always",
-        )
-
-    async def run(
-        self, action_input: ActionInput[SaveContactParameters]
-    ) -> ActionOutput[SaveContactCenterResponse]:
-        twilio_call_sid = self.get_twilio_sid(action_input)
-        logger.debug(f"Twilio Call SID: {twilio_call_sid}")
-
-        twilio_client = self.conversation_state_manager.create_twilio_client()
-
-        url = f"https://api.twilio.com/2010-04-01/Accounts/{twilio_client.get_telephony_config().account_sid}/Calls/{twilio_call_sid}.json"
-
-        async with AsyncRequestor().get_session() as session:
-            async with session.get(url, auth=twilio_client.auth) as response:
-                if response.status != 200:
-                    logger.error(
-                        f"Failed to get call details: {response.status} {response.reason}"
-                    )
-                    success = False
-                    agent_message = "Failed to get caller details"
-                    message = {
-                        "result": {"success": False},
-                        "agent_message": agent_message,
-                    }
-                    return ActionOutput(
-                        action_type=action_input.action_config.type,
-                        response=SaveContactCenterResponse(
-                            success=success, result=message
-                        ),
-                    )
-                else:
-                    call_details = await response.json()
-                    logger.debug(f"Call Details: {call_details}")
-
-        phone_number = call_details.get("from", "")
-        logger.debug(f"Extracted Phone Number: {phone_number}")
-
-        if not phone_number:
-            logger.error("No phone number found in call details.")
-            success = False
-            agent_message = "No phone number found in call details"
-            message = {
-                "result": {"success": False},
-                "agent_message": agent_message,
-            }
-
-            return ActionOutput(
-                action_type=action_input.action_config.type,
-                response=SaveContactCenterResponse(success=success, result=message),
-            )
-
-        server_url = os.environ.get("PORTAL_URL")
-        headers = {
-            "Content-Type": "application/json",
-            "X-Auth-Token": os.environ.get("PORTAL_AUTH_TOKEN"),
-            "X-User-Id": os.environ.get("PORTAL_USER_ID"),
-        }
-
-        if (
-            not server_url
-            or not headers["X-Auth-Token"]
-            or not headers["X-User-Id"]
-        ):
-            logger.error(
-                "Missing environment variables for PORTAL_URL, PORTAL_AUTH_TOKEN, or PORTAL_USER_ID."
-            )
-            success = False
-            agent_message = "Failed to save contact information"
-            message = {
-                "result": {"success": False},
-                "agent_message": agent_message,
-            }
-            return ActionOutput(
-                action_type=action_input.action_config.type,
-                response=SaveContactCenterResponse(success=success, result=message),
-            )
-
-        # **Fixed Attribute Access Here**
-        caller_name = action_input.params.caller_name
-        email_address = action_input.params.email_address
-
-        success, result = await add_to_contact_center(
-            server_url, headers, phone_number, caller_name, email_address
-        )
-
-        if success:
-            agent_message = "Contact saved successfully"
-        else:
-            agent_message = f"Failed to save contact: {result}"
-
-        message = {
-            "result": {"success": success, "data": result},
-            "agent_message": agent_message,
-        }
-
-        logger.debug(f"Final Message: {agent_message}")
-
-        return ActionOutput(
-            action_type=action_input.action_config.type,
-            response=SaveContactCenterResponse(success=success, result=message),
-        )
+FUNCTION_DESCRIPTION = "Adds caller information to the contact center"
+QUIET = False
+IS_INTERRUPTIBLE = False
+SHOULD_RESPOND: Literal["always"] = "always"
 
 
 async def add_to_contact_center(server_url, headers, phone, caller_name, email_address):
@@ -242,3 +165,129 @@ async def add_to_contact_center(server_url, headers, phone, caller_name, email_a
     else:
         logger.debug("Contact already exists")
         return True, {"message": "Contact already exists"}
+
+
+class TwilioAddToContactCenter(
+    TwilioPhoneConversationAction[
+        AddToContactCenterVocodeActionConfig,
+        AddToContactCenterParameters,
+        AddToContactCenterResponse,
+    ]
+):
+    description: str = FUNCTION_DESCRIPTION
+    response_type: Type[AddToContactCenterResponse] = AddToContactCenterResponse
+    conversation_state_manager: TwilioPhoneConversationStateManager
+
+    @property
+    def parameters_type(self) -> Type[AddToContactCenterParameters]:
+        if (
+            self.action_config.phone_number
+            and self.action_config.caller_name
+            and self.action_config.email_address
+        ):
+            return AddToContactCenterEmptyParameters
+        else:
+            return AddToContactCenterRequiredParameters
+
+    def __init__(
+        self,
+        action_config: AddToContactCenterVocodeActionConfig,
+    ):
+        super().__init__(
+            action_config,
+            quiet=QUIET,
+            is_interruptible=IS_INTERRUPTIBLE,
+            should_respond=SHOULD_RESPOND,
+        )
+
+    async def run(
+        self, action_input: ActionInput[AddToContactCenterParameters]
+    ) -> ActionOutput[AddToContactCenterResponse]:
+        phone_number = self.action_config.get_phone_number(action_input)
+        caller_name = self.action_config.get_caller_name(action_input)
+        email_address = self.action_config.get_email_address(action_input)
+
+        sanitized_phone_number = sanitize_phone_number(phone_number)
+
+        server_url = os.environ.get("PORTAL_URL")
+        headers = {
+            "Content-Type": "application/json",
+            "X-Auth-Token": os.environ.get("PORTAL_AUTH_TOKEN"),
+            "X-User-Id": os.environ.get("PORTAL_USER_ID"),
+        }
+
+        success, response = await add_to_contact_center(
+            server_url,
+            headers,
+            sanitized_phone_number,
+            caller_name,
+            email_address,
+        )
+
+        return ActionOutput(
+            action_type=action_input.action_config.type,
+            response=AddToContactCenterResponse(success=success, message=str(response)),
+        )
+
+
+class VonageAddToContactCenter(
+    VonagePhoneConversationAction[
+        AddToContactCenterVocodeActionConfig,
+        AddToContactCenterParameters,
+        AddToContactCenterResponse,
+    ]
+):
+    description: str = FUNCTION_DESCRIPTION
+    response_type: Type[AddToContactCenterResponse] = AddToContactCenterResponse
+    conversation_state_manager: VonagePhoneConversationStateManager
+
+    @property
+    def parameters_type(self) -> Type[AddToContactCenterParameters]:
+        if (
+            self.action_config.phone_number
+            and self.action_config.caller_name
+            and self.action_config.email_address
+        ):
+            return AddToContactCenterEmptyParameters
+        else:
+            return AddToContactCenterRequiredParameters
+
+    def __init__(
+        self,
+        action_config: AddToContactCenterVocodeActionConfig,
+    ):
+        super().__init__(
+            action_config,
+            quiet=QUIET,
+            is_interruptible=IS_INTERRUPTIBLE,
+            should_respond=SHOULD_RESPOND,
+        )
+
+    async def run(
+        self, action_input: ActionInput[AddToContactCenterParameters]
+    ) -> ActionOutput[AddToContactCenterResponse]:
+        phone_number = self.action_config.get_phone_number(action_input)
+        caller_name = self.action_config.get_caller_name(action_input)
+        email_address = self.action_config.get_email_address(action_input)
+
+        sanitized_phone_number = sanitize_phone_number(phone_number)
+
+        server_url = os.environ.get("PORTAL_URL")
+        headers = {
+            "Content-Type": "application/json",
+            "X-Auth-Token": os.environ.get("PORTAL_AUTH_TOKEN"),
+            "X-User-Id": os.environ.get("PORTAL_USER_ID"),
+        }
+
+        success, response = await add_to_contact_center(
+            server_url,
+            headers,
+            sanitized_phone_number,
+            caller_name,
+            email_address,
+        )
+
+        return ActionOutput(
+            action_type=action_input.action_config.type,
+            response=AddToContactCenterResponse(success=success, message=str(response)),
+        )
